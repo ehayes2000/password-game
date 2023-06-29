b467dd42e5c4b74acbdbf002d53ec1f1b2b093f8
eric <eqhayes@protonmail.com>
2023-06-29 16:11:05 -0700

    clean rule component

--- src/Rule.js
+++ src/Rule.js
@@ -1,27 +1,12 @@
-const Rule = ({ rule, name, satisfied, hidden }) => {
-    if (satisfied) {
-        return (
-        <div className={`flex flex-col overflow-hidden rounded-xl font-sans font-semibold shadow-slate-300 shadow-md m-6 ${hidden ? 'hidden' : ''}`}>
-            <div className="flex-grow p-2 pl-6 bg-green-300">
-                Rule {name}
-            </div>
-            <div className="bg-green-200 p-4">
-                {rule}
-            </div>
+const Rule = ({ rule, name, satisfied, hidden }) => (
+    <div className={`flex flex-col overflow-hidden rounded-xl font-sans font-semibold shadow-slate-300 shadow-md m-6 ${hidden ? 'hidden' : ''}`}>
+        <div className={`flex-grow p-2 pl-6 ${satisfied ? 'bg-green-300' : 'bg-rose-300'}`}>
+            Rule {name}
         </div>
-        );
-    }
-
-    return (
-        <div className={`flex flex-col overflow-hidden rounded-xl font-sans font-semibold shadow-slate-300 shadow-md m-6 ${hidden ? 'hidden' : ''}`}>
-            <div className="flex-grow p-2 pl-6 bg-rose-300">
-                Rule {name}
-            </div>
-            <div className="bg-rose-200 p-4">
-                {rule}
-            </div>
+        <div className={`${satisfied ? 'bg-green-200': 'bg-rose-200'} p-4`}>
+            {rule}
         </div>
-    );
-}
-
+    </div>
+)
+     
 export default Rule;    
