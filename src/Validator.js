039f4f130eecda835e3fc621d93408e7f9d3fb44
eric <eqhayes@protonmail.com>
2023-06-29 22:33:15 -0700

    fix bugs with cards display out of order

--- src/Validator.js
+++ src/Validator.js
@@ -4,61 +4,66 @@ import EnterPassword from "./EnterPassword"
  
 const Validator = () => {
     const [password, setPassword] = useState("");
+    const [latestRule, setLatestRule] = useState(0);
     const [rules, setRules] = useState([
         {
             name: 1,
-            rule: "Password must be at least 5 characters long",
+            rule: "Your password must be at least 5 characters long.",
             isValid: password => password.length >= 5,
             isDisplayed: false,
-            satisfied: false, 
-            wasValid: false,
+            satisfied: false,    
         },    
         {
             name: 2,
-            rule: "Password must contain a number",
+            rule: "Your password must contain a number.",
             isValid: password => /\d/.test(password),
             isDisplayed: false,
             satisfied: false,
-            wasValid: false,
         },
         {
-            name: 2,
-            rule: "Password must be lenth 10",
-            isValid: password => password.length === 10,
+            name: 3, 
+            rule: "Your password must include an uppercase letter.",
+            isValid: password => /[A-Z]/.test(password),
+            isDisplayed: false, 
+            satisfied: false,
+           
+        },
+        {
+            name: 4, 
+            rule: "Your password must include an special character.",
+            isValid: password => /[^A-Za-z0-9]/.test(password),
+            isDisplayed: false, 
+            satisfied: false,
+           
+        },
+        {
+            name: 5, 
+            rule: "The digits of your password must sum to 25.",
+            isValid: password => 
+                password.match(/\d/g)?.reduce((acc, i) => acc + parseInt(i), 0) === 25,
             isDisplayed: false,
             satisfied: false,
-            wasValid: false,
-        },
+        }
 
         // ... add more rules here ...
     ]);
 
     useEffect(() => {
         // First update all 'satisfied' and 'wasValid' properties
-        let updatedRules = rules.map((rule, index) => {
-            const isRuleValid = rule.isValid(password);
-            return {
-                ...rule,
-                satisfied: isRuleValid,
-                wasValid: isRuleValid || rule.wasValid,
-            };
-        });
-        
-        // Then update 'isDisplayed' properties
-        setRules(updatedRules.map((rule, index) => {
-            let shouldRuleBeDisplayed = updatedRules[index - 1] && updatedRules[index - 1].wasValid;
-            if (password.length > 0 && index == 0) {
-                return {
-                    ...rule, 
-                    isDisplayed: true, 
-                }
+        let updatedRules = [...rules];
+        for (let i = 0; i <= latestRule; i++ ){
+            if (i >= rules.length)
+                break;
+            const isRuleSatisfied = rules[i].isValid(password);
+            updatedRules[i].satisfied = isRuleSatisfied;
+            updatedRules[i].isDisplayed = true;
+            if (i === latestRule && isRuleSatisfied){
+                setLatestRule(latestRule + 1);
+                break;
             }
-            return {
-                ...rule,
-                isDisplayed: shouldRuleBeDisplayed,
-            };
-        }));
-    }, [password]);
+        }   
+        setRules(updatedRules);
+    }, [password, latestRule]);
     
 
     return (
@@ -68,8 +73,6 @@ const Validator = () => {
             <span className="text-6xl inline-block relative" style={{top: '0.2em'}}>*</span> The Password Game
             </h1>
             <EnterPassword className="block" setPassword={setPassword} />
-          
-         
             {rules.slice().reverse()
             .map((r, i) => (
                 <Rule name={r.name} rule={r.rule} hidden={!r.isDisplayed} satisfied={r.satisfied}/>
