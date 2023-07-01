5d9d831d346fc90ccef36a8bb9e863424b02fff8
eric <eqhayes@protonmail.com>
2023-06-30 18:56:34 -0700

    make captcha better

--- src/components/CaptchaRule.js
+++ src/components/CaptchaRule.js
@@ -29,12 +29,17 @@ const CaptchaRule = ({ images, setCaptcha }) => {
     if (!selectedImage) return null;
 
     return (
-        <div>
-            <img src={process.env.PUBLIC_URL + selectedImage} alt="random" />
-            <button onClick={handleRefresh}>Refresh</button>
+        <div className="flex items-center">
+            <img className="rounded-xl mr-6" src={process.env.PUBLIC_URL + selectedImage} alt="random" />
+            <svg className="w-10 text-gray-800 hover:text-gray-600 transition-colors duration-50" onClick={handleRefresh} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+            </svg>
         </div>
     );
 }
+
+
+
 
 export default CaptchaRule;
 
